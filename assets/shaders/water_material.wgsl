a5352f8293ab78838222b89f4eefb309a04f84fb
Magnus Kroner <magnus.kroner@gmail.com>
2024-03-11 15:24:45 +0100

    normals

--- assets/shaders/water_material.wgsl
+++ assets/shaders/water_material.wgsl
@@ -1,13 +1,20 @@
 #import bevy_pbr::{
     prepass_utils,
+    pbr_fragment::pbr_input_from_standard_material,
     forward_io::VertexOutput,
+    forward_io::FragmentOutput,
+    pbr_functions::{apply_pbr_lighting, main_pass_post_lighting_processing},
+    pbr_functions::alpha_discard,
     mesh_view_bindings::view,
+    mesh_view_bindings::globals,
 }
 
-@group(2) @binding(0) var<uniform> shallow_water: vec4<f32>;
-@group(2) @binding(1) var<uniform> deep_water: vec4<f32>;
-@group(2) @binding(2) var<uniform> depth: f32;
-@group(2) @binding(3) var<uniform> strength: f32;
+@group(2) @binding(50) var<uniform> shallow_water: vec4<f32>;
+@group(2) @binding(51) var<uniform> deep_water: vec4<f32>;
+@group(2) @binding(52) var<uniform> depth: f32;
+@group(2) @binding(53) var<uniform> strength: f32;
+@group(2) @binding(54) var main_normal_texture: texture_2d<f32>;
+@group(2) @binding(55) var main_normal_sampler: sampler;
 
 fn depth_to_linear(d: f32) -> f32 {
     return -view.projection[3][2] / d;
@@ -21,7 +28,36 @@ fn fragment(
     let depth_buffer = bevy_pbr::prepass_utils::prepass_depth(mesh.position, sample_index);
     let depth_view_diff = depth_to_linear(mesh.position.z) + depth - (depth_to_linear(depth_buffer));
     let beers_law = exp(-depth_view_diff * strength);
-    let out = mix(deep_water, shallow_water, beers_law);
+    let water_color = mix(deep_water, shallow_water, beers_law);
 
-    return out;
+    let normal_scale = 2.0;
+    var in = mesh;
+
+    let speed = 0.1;
+    let t_0 = sin(globals.time * speed);
+    let t_1 = globals.time * speed * 0.5;
+
+    let normal = in.world_normal;
+    let tangent = in.world_tangent.xyz;
+    let bitangent = cross(tangent, normal);
+
+    var mapped_normal = textureSample(main_normal_texture, main_normal_sampler, mesh.uv * normal_scale + vec2(t_0, 0.0)).rgb;
+    mapped_normal = mapped_normal * 2.0 - 1.0;
+    mapped_normal = mapped_normal.x * tangent + mapped_normal.y * bitangent + mapped_normal.z * normal;
+    in.world_normal = normalize(mapped_normal);
+
+    var mapped_normal2 = textureSample(main_normal_texture, main_normal_sampler, mesh.uv * normal_scale + vec2(0.0, t_1)).rgb;
+    mapped_normal2 = mapped_normal2 * 2.0 - 1.0;
+    mapped_normal2 = mapped_normal2.x * tangent + mapped_normal2.y * bitangent + mapped_normal2.z * normal;
+    in.world_normal = normalize(mapped_normal+ mapped_normal2);
+
+    var pbr_input = pbr_input_from_standard_material(in, false);
+    pbr_input.material.base_color = water_color;
+    pbr_input.material.base_color = alpha_discard(pbr_input.material, pbr_input.material.base_color);
+    var out: FragmentOutput;
+    out.color = apply_pbr_lighting(pbr_input);
+    out.color = main_pass_post_lighting_processing(pbr_input, out.color);
+
+    return out.color;
+    //return vec4(mapped_normal, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::f32::consts::PI;
 
 use bevy::{
+    pbr::{ExtendedMaterial, MaterialExtension},
     prelude::*,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::{
+        render_resource::{AsBindGroup, ShaderRef},
+        texture::{ImageAddressMode, ImageLoaderSettings, ImageSampler, ImageSamplerDescriptor},
+    },
 };
 mod fly_cam;
 
@@ -10,7 +14,7 @@ fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins,
-            MaterialPlugin::<WaterMaterial> {
+            MaterialPlugin::<ExtendedMaterial<StandardMaterial, WaterMaterial>> {
                 prepass_enabled: false,
                 ..default()
             },
@@ -25,10 +29,10 @@ fn spawn_light(mut cmd: Commands) {
     cmd.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 10_000_000.0,
-            range: 1000.0,
+            range: 100.0,
             ..default()
         },
-        transform: Transform::from_xyz(0.0, 10.0, 0.0),
+        transform: Transform::from_xyz(0.0, 4.0, 0.0),
         ..default()
     });
 }
@@ -36,16 +40,37 @@ fn spawn_light(mut cmd: Commands) {
 fn spawn_water(
     mut cmd: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<WaterMaterial>>,
+    mut materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, WaterMaterial>>>,
     mut std_materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
+    let sampler_desc = ImageSamplerDescriptor {
+        address_mode_u: ImageAddressMode::Repeat,
+        address_mode_v: ImageAddressMode::Repeat,
+        ..default()
+    };
+
+    let settings = move |s: &mut ImageLoaderSettings| {
+        s.sampler = ImageSampler::Descriptor(sampler_desc.clone());
+    };
+
     cmd.spawn(MaterialMeshBundle {
-        mesh: meshes.add(Plane3d::default().mesh().size(20., 20.)),
-        material: materials.add(WaterMaterial {
-            shallow_water: Color::rgb_u8(210, 162, 255),
-            deep_water: Color::rgb_u8(103, 64, 128),
-            depth: 0.1,
-            strength: 0.5,
+        mesh: meshes.add(Mesh::from(Plane3d::default().mesh().size(20., 20.)).with_generated_tangents().unwrap()),
+        material: materials.add(ExtendedMaterial {
+            base: StandardMaterial { 
+                perceptual_roughness: 0.22,
+                // metallic: 1.0,
+                // normal_map_texture: Some(asset_server.load_with_settings("textures/normals/water.png", settings.clone())),
+                // specular_transmission: 1.0,
+                ..default() },
+            extension: WaterMaterial {
+                shallow_water: Color::rgb_u8(210, 162, 255),
+                deep_water: Color::rgb_u8(103, 64, 128),
+                depth: 0.1,
+                strength: 0.5,
+                main_normal_texture: asset_server
+                    .load_with_settings("textures/normals/water20.png", settings),
+            },
         }),
         ..default()
     });
@@ -68,24 +93,32 @@ fn spawn_water(
     cmd.spawn(PbrBundle {
         mesh: meshes.add(Cuboid::default()),
         material: std_materials.add(Color::SILVER),
+        transform: Transform::from_scale(Vec3::new(1.0, 3.0, 1.0)),
         ..default()
     });
 }
 
 #[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
 struct WaterMaterial {
-    #[uniform(0)]
+    #[uniform(50)]
     shallow_water: Color,
-    #[uniform(1)]
+    #[uniform(51)]
     deep_water: Color,
-    #[uniform(2)]
+    #[uniform(52)]
     depth: f32,
-    #[uniform(3)]
+    #[uniform(53)]
     strength: f32,
+    #[texture(54)]
+    #[sampler(55)]
+    main_normal_texture: Handle<Image>,
 }
 
-impl Material for WaterMaterial {
+impl MaterialExtension for WaterMaterial {
     fn fragment_shader() -> ShaderRef {
         "shaders/water_material.wgsl".into()
+    }
+
+    fn vertex_shader() -> ShaderRef {
+        ShaderRef::Default
     }
 }
